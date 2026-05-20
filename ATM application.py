621586177c0419f0bfe55fcdb65cf4507a148ab9
Hasini Vemuri <vemurihasini@gmail.com>
2026-05-20 16:07:01 +0530

    Update ATM application.py

--- ATM application.py
+++ ATM application.py
@@ -1,24 +1,38 @@
 #ATM APPLICATION
 
-amount=100000
-a=input("insert the card")
-if a=="c":
+amount = 100000
+
+a = input("Insert the card: ")
+
+if a == "c":
     print("Welcome Hasini")
-else :
+
+    b = int(input("Enter the password: "))
+
+    if b == 1234:
+        print("Login successful")
+
+        c = int(input("Choose the option\n1. Account Balance\n2. Withdraw\n"))
+
+        if c == 1:
+            print("Your Account Balance is", amount)
+
+        elif c == 2:
+            d = int(input("Amount to be Withdrawn: "))
+
+            if d <= amount:
+                print("The remaining balance is", amount - d)
+            else:
+                print("Insufficient balance")
+
+        else:
+            print("Option not found")
+
+    else:
+        print("Invalid password")
+
+else:
     print("Invalid card")
-b=int(input("Enter the password"))
-if b==1234:
-    print("login successful")
-else:
-    print("login invalid")
-c=int(input("choose the option 1.Account Balance 2.Withdraw"))
-if c==1:
-    print("Your Account Balance is",amount)
-elif c==2:
-    d=int(input("Amount to be Withdrawn"))
-    print("The remaining balance is",amount-d)
-else:
-    print("Option not found")   
         
         
 
